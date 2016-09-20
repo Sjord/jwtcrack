6c66482cd081252d2704688e60a091bfea1cad15
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-09-20 14:25:46 +0200

    Improve crackjwt.py
    So that it is easier to use and understand

--- crackjwt.py
+++ crackjwt.py
@@ -1,20 +1,54 @@
 #!/usr/bin/python
 
-import jwt
+from jwt import decode, InvalidTokenError, DecodeError
 import sys
 
-encoded = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyMzQ1Njc4OTAsIm5hbWUiOiJKb2huIERvZSIsImFkbWluIjp0cnVlfQ.eoaDVGTClRdfxUZXiPs3f8FmJDkDE_VCQFXqKxpLsts'
 
-with open(sys.argv[1]) as secrets:
-    for secret in secrets:
-        try:
-            payload = jwt.decode(encoded, secret.rstrip(), algorithms=['HS256'])
-            print("Success", secret)
-        except jwt.InvalidTokenError as e:
-            if e.args[0] == 'Signature verification failed':
+def is_jwt(jwt):
+    parts = jwt.split('.')
+    if len(parts) != 3:
+        return False
+
+    return True
+
+
+def read_jwt(jwt):
+    if not is_jwt(jwt):
+        with open(jwt) as fp:
+            jwt = fp.read().strip()
+
+    if not is_jwt(jwt):
+        raise RuntimeError("Parameter %s is not a valid JWT" % jwt)
+
+    return jwt
+
+
+def crack_jwt(jwt, dictionary):
+    with open(dictionary) as fp:
+        for secret in fp:
+            secret = secret.rstrip()
+
+            try:
+                decode(jwt, secret, algorithms=['HS256'])
+                return secret
+            except DecodeError:
+                # Signature verification failed
                 pass
-            else:
-                print("Success", secret)
-                break
-        except jwt.ExpiredSignatureError:
-            print("Token expired")
+            except InvalidTokenError:
+                # Signature correct, something else failed
+                return secret
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: %s [JWT or JWT filename] [dictionary filename] " % sys.argv[0])
+    else:
+        jwt = read_jwt(sys.argv[1])
+        print("Cracking JWT %s" % jwt)
+        result = crack_jwt(jwt, sys.argv[2])
+        if result:
+            print("Found secret key:", result)
+        else:
+            print("Key not found")
+
+
